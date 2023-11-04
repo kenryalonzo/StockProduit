1ae533fb4d46c2d4b403b63eef8662a4caf9b24a
GHOST <kenryalonzo@gmail.com>
2023-11-04 17:52:41 +0100

    I did this little project for fun, the description of this commit already says it's all disappointed, so no need to add more.

---
+++ README.md
@@ -0,0 +1,3 @@
+#StockProduit
+Hello friends today I come with a program which partially manages the products of a stock, and as you guessed it is written in C language.
+Well it is true that I will make a commit very soon to correct certain details.
---
+++ produit.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+
+    int code_produit;
+    char designation[10];
+    int quantite_stock;
+
+#define TAILLE_STOCK 10
+
+}Produit;
+
+typedef struct {
+
+    Produit tab[TAILLE_STOCK];   
+}Stock;
+
+Produit lireProduit();
+void afficheStock(Stock s);
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
+
+Produit lireProduit() {
+
+    Produit product;
+    scanf("%d %s %d \n\n", &product.code_produit, product.designation, &product.quantite_stock);
+    return product;
+}
+
+void afficheStock(Stock s) {
+    
+    for (int i = 0; i < TAILLE_STOCK; i++) {
+        printf("%d\t %s\t %d\n", s.tab[i].code_produit, s.tab[i].designation, s.tab[i].quantite_stock);
+    }
+}
+
+int enStock(Stock s, int code) {
+
+    int trouve = 0, i = 0;
+    while (trouve == 0 && i < TAILLE_STOCK) {
+        
+        if (s.tab[i].code_produit == code) {
+            trouve = 1;
+        }
+        i++;
+
+        return trouve;
+    }
+    
+    
+}
